60c5083443952742cc7c83aa208be627cb3904cf
Jaakko Hannikainen <jgke@jgke.fi>
2016-12-15 00:22:23 +0200

    Add simple test for Type

---
+++ src/test/java/fi/jgke/tagger/domain/TypeTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.jgke.tagger.domain;
+
+import java.util.UUID;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jgke
+ */
+public class TypeTest {
+
+    @Test
+    public void testSetGetValue() {
+        String value = UUID.randomUUID().toString();
+        Tag instance = new Tag();
+        instance.setValue(value);
+        assertEquals(value, instance.getValue());
+    }
+
+}
