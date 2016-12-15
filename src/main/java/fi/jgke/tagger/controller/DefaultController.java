f77893d1dbb6b4fc52e7fb37dbac5c6bec88e8e1
Jaakko Hannikainen <jgke@jgke.fi>
2016-12-15 18:27:31 +0200

    Add a basic index page

--- src/main/java/fi/jgke/tagger/controller/DefaultController.java
+++ src/main/java/fi/jgke/tagger/controller/DefaultController.java
@@ -1,16 +1,22 @@
 package fi.jgke.tagger.controller;
 
+import fi.jgke.tagger.repository.SourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
 
+    @Autowired
+    SourceRepository sourceRepository;
+
     @RequestMapping("/")
-    @ResponseBody
-    public String handleDefault() {
-        return "Hello world!";
+    public String handleDefault(Model model) {
+        model.addAttribute("sources", sourceRepository.findAll());
+        return "index";
     }
-    
+
 }
