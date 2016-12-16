6f39242514ea1a3e78afd7b9ca2419554282ca3b
Jaakko Hannikainen <jgke@jgke.fi>
2016-12-16 17:07:27 +0200

    Add Apache 2 license header to all files

--- src/main/java/fi/jgke/tagger/config/SwaggerConfig.java
+++ src/main/java/fi/jgke/tagger/config/SwaggerConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Jaakko Hannikainen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package fi.jgke.tagger.config;
 
 import org.springframework.context.annotation.Bean;
@@ -11,12 +26,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
--- src/test/java/fi/jgke/tagger/controller/HealthTest.java
+++ src/test/java/fi/jgke/tagger/controller/HealthTest.java
@@ -1,25 +1,29 @@
+/*
+ * Copyright 2016 Jaakko Hannikainen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package fi.jgke.tagger.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fi.jgke.tagger.domain.Tag;
-import fi.jgke.tagger.domain.Type;
-import fi.jgke.tagger.repository.TagRepository;
-import fi.jgke.tagger.repository.TypeRepository;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
